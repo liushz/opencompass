47186b9ef499d96e7f6199244cd7b48bf1381bd2
liushz <qq1791167085@163.com>
2024-10-29 07:07:51 +0000

    update PRM800k dataset

--- opencompass/configs/datasets/prm800k/prm800k_gen.py
+++ opencompass/configs/datasets/prm800k/prm800k_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .prm800k_gen_b54e08 import prm800k_datasets  # noqa: F401, F403
+    from .prm800k_gen_c848ae import prm800k_datasets  # noqa: F401, F403
